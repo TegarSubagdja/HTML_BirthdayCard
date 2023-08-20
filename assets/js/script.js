a5b21ebe0b113188590eb3cfa5f5ec3f89ee2b81
Tegar Subagdja <kingtegar1510@gmail.com>
2023-08-20 14:54:30 +0700

    fix layout

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.querySelector("body");
+
+    body.addEventListener("click", function () {
+        window.location.href = "home.html";
+    });
+});
