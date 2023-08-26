0f0abf5b3eb96660a1c9b1c21013b094b637af8f
Tegar Subagdja <kingtegar1510@gmail.com>
2023-08-26 20:27:50 +0700

    update modal

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,17 +1,11 @@
-const button = document.getElementById('show');
-const div = document.getElementsByClassName('modal-part')[0];
+const button = document.getElementById('button');
+
 button.addEventListener('click', () => {
-  setTimeout(() => {
-    button.style.display = 'none';
-    div.classList.remove('modal-part');
     var audio = new Audio("assets/musik/musik.mp3");
     audio.oncanplaythrough = function () {
-      audio.play();
+        audio.play();
     }
-    audio.loop = true;
     audio.onended = function () {
-      audio.play();
+        audio.play();
     }
-  }, 1000);
-});
-
+})
