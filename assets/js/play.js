fe30faace73446ede523a106ff0fe4b330fe2a40
Tegar Subagdja <kingtegar1510@gmail.com>
2023-08-28 11:17:16 +0700

    adding click android

--- assets/js/play.js
+++ assets/js/play.js
@@ -3,16 +3,19 @@ var touchStartTime;
 
 document.addEventListener('touchstart', function (event) {
     touchStartTime = new Date().getTime();
+    setTimeout(function () {
+        document.addEventListener('touchend', handleTouchEnd);
+    }, 2000);
 });
 
-document.addEventListener('touchend', function (event) {
+function handleTouchEnd(event) {
+    document.removeEventListener('touchend', handleTouchEnd);
     var touchEndTime = new Date().getTime();
     var touchDuration = touchEndTime - touchStartTime;
-
     if (touchDuration >= 2000) {
         alert('Anda menahan layar selama 2 detik atau lebih.');
     }
-});
+}
 
 document.addEventListener('keydown', function (Event) {
     if (Event.keyCode == 83) {
